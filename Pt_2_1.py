ef190f2bb011d694c8c689c6a147349716d370ca
ArinaBulygina <bulyginaarina2@gmail.com>
2023-07-07 00:10:14 +0900

    Pt_2_1: Guess the color
    The user guesses the color that the program has guessed
    using the suggested hint

---
+++ Pt_2_1.py
@@ -0,0 +1,25 @@
+import random
+
+colors = "Красный Синий Жёлтый Зелёный Чёрный"
+print(colors)
+string = colors.split()
+
+color = input("Введите цвет из списка, который по вашему мнению выбрала программа: ")
+index_pc_color = int(random.choice("01234"))
+while True:
+    if color != string [index_pc_color]:
+        if string[index_pc_color] == "Красный":
+            print("Неправильно! \nПодсказка: Такого цвета пламя")
+        if string[index_pc_color] == "Синий":
+            print("Неправильно! \nПодсказка: Такого цвета море")
+        if string[index_pc_color] == "Жёлтый":
+            print("Неправильно! \nПодсказка: Такого цвета солнце")
+        if string[index_pc_color] == "Зелёный":
+            print("Неправильно! \nПодсказка: Такого цвета трава")
+        if string[index_pc_color] == "Чёрный":
+            print("Неправильно! \nПодсказка: Такого цвета тень")
+    else:
+        print("Отлично!")
+        break
+    print(colors)
+    color = (input("Попробуйте снова, введите цвет: "))
