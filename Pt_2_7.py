ec1d3f9a4cd7f802850e3b0cf4448be5187f2dbc
ArinaBulygina <bulyginaarina2@gmail.com>
2023-07-07 01:44:48 +0900

    Pt_2_7: Sum of negative numbers
    The program sums the numbers entered from the keyboard
    as long as they are negative.

---
+++ Pt_2_7.py
@@ -0,0 +1,8 @@
+sum=0
+while True:
+    number = int(input("Введите число: "))
+    if number < 0:
+        sum += number
+    else:
+        print("Вы ввели неотрицательное число. Полученная сумма: ", sum)
+        break
