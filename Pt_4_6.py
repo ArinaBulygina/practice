21e79321675bb9924a6c9d9f94a2030f958aa182
ArinaBulygina <bulyginaarina2@gmail.com>
2023-07-15 03:12:55 +0900

    Pt_4_6: Caesar 's cipher
    The program encodes the input string using the Caesar cipher

---
+++ Pt_4_6.py
@@ -0,0 +1,13 @@
+string = input("Введите строку для кодировки: ")
+a = 'abcdefghijklm'
+b = 'nopqrstuvwxyz'
+for i in range(len(string)):
+    if string[i] in a:
+        for j in range(len(a)):
+            if string[i] == a[j]:
+                string = string[:i] + b[j] + string[i+1:]
+    else:
+        for k in range(len(b)):
+            if string[i] == b[k]:
+                string = string[:i] + a[k] + string[i+1:]
+print("Полученная строка: ", string)
