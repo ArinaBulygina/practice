2e77326b8e98c951788de3015db3395669a91e72
ArinaBulygina <bulyginaarina2@gmail.com>
2023-07-15 01:45:54 +0900

    Pt_4_2: All palindromes in a string
    The program finds all substrings in a given string that are palindromes

---
+++ Pt_4_2.py
@@ -0,0 +1,10 @@
+string = input("Введите строку: ")
+palindromes = []
+for i in range(len(string)):
+    for j in range(i+1, len(string)+1):
+        stroke = string[i:j]
+        reversed_stroke = "".join(reversed(stroke))
+        if stroke == reversed_stroke and len(stroke) > 1:
+            palindromes.append(stroke)
+
+print("Все палиндромы: ", palindromes)
