a187c0b9b36c90301b161ddf6795eb093ca07191
ArinaBulygina <138182782+ArinaBulygina@users.noreply.github.com>
2023-07-20 02:37:24 +0900

    Pt_4_2: Fixing pycodestyle errors

--- Pt_4_2.py
+++ Pt_4_2.py
@@ -1,7 +1,7 @@
 string = input("Введите строку: ")
 palindromes = []
 for i in range(len(string)):
-    for j in range(i+1, len(string)+1):
+    for j in range(i + 1, len(string) + 1):
         stroke = string[i:j]
         reversed_stroke = "".join(reversed(stroke))
         if stroke == reversed_stroke and len(stroke) > 1:
