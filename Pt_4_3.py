6611f624fcd5443cbfe6d57d4b5236e114b2885e
ArinaBulygina <138182782+ArinaBulygina@users.noreply.github.com>
2023-07-20 02:38:05 +0900

    Pt_4_3: Fixing pycodestyle errors

--- Pt_4_3.py
+++ Pt_4_3.py
@@ -7,7 +7,7 @@ def check_simple(n):
 
 a, b = int(input("Введите начало диапазона: ")), int(input("Введите конец диапазона: "))
 simple_numbers = []
-for i in range(a, b+1):
+for i in range(a, b + 1):
     if check_simple(i):
         simple_numbers.append(i)
 print("Все простые числа: ", *simple_numbers)
