b556a983485e76f77ca6fa5e298040888bf2e563
ArinaBulygina <bulyginaarina2@gmail.com>
2023-07-22 02:33:21 +0900

    Pt_5_2: Search for books by author and add new books
    The program creates a custom menu where there are commands: Search for books by author and Add new books to the list, as well as shutdown

---
+++ Pt_5_2.py
@@ -0,0 +1,42 @@
+import csv
+
+
+def add_books():
+    kol = int(input("Введите количество строк, которые будут добавлены: "))
+    new_books = []
+    for i in range(kol):
+        book, author, year = input("Введите название книги, автора и год издания через пробел: ").split()
+        new_books.append([book, author, year])
+    with open("books.csv", "a", newline='') as file:
+        writer = csv.writer(file, delimiter=";")
+        writer.writerows(new_books)
+    print("Книги успешно добавлены в список!")
+
+
+def find_books():
+    found_books = []
+    author = input("Введите автора книги: ")
+    print(f"Книги автора - {author}: ")
+    with open("books.csv", newline='') as file:
+        reader = csv.DictReader(file, delimiter=";")
+        for row in reader:
+            if author == row["Author"]:
+                found_books.append(row)
+    if not found_books:
+        print("В списке нет книг данного автора")
+    else:
+        for row in found_books:
+            print(row["Book"], "|", row["Year of release"])
+
+
+while True:
+    choose = int(input("\nВыберите номер команды:\n"
+                       "1. Добавление новых книг\n"
+                       "2. Поиск книг по автору\n"
+                       "3. Завершение работы\n"))
+    if choose == 1:
+        add_books()
+    elif choose == 2:
+        find_books()
+    elif choose == 3:
+        break
