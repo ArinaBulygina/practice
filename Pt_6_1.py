891e6cf9ecb54361e7524d2d20a5cc9916a2df54
ArinaBulygina <bulyginaarina2@gmail.com>
2023-07-22 13:21:04 +0900

    Pt_6_1: Top 10 popular songs by the artist
    The program displays the top 10 popular songs of the artist from the Yandex Music service

---
+++ Pt_6_1.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+from loguru import logger
+
+
+link = input("Вставьте ссылку на исполнителя на сервисе Yandex Music: ")
+
+try:
+    accession = requests.get(link)
+    soup = BeautifulSoup(accession.text, "html.parser")
+
+    name_of_artist = soup.find("h1", class_="page-artist__title typo-h1 typo-h1_big").text
+    print(f"Топ 10 песен {name_of_artist} на сервисе Yandex Music: ")
+    k = 1
+    all_tracks = soup.find_all("a", class_="d-track__title deco-link deco-link_stronger")
+    for track in all_tracks[:10]:
+        print(f"{k}. {track.text}")
+        k += 1
+except requests.exceptions.MissingSchema:
+    logger.error("Invalid URL! Try again, following the example https://music.yandex.ru/artist/0000000/tracks")
+
+except requests.exceptions.InvalidSchema:
+    logger.error(f"No connection adapters were found for {link}. Use the right schema"
+                 f" and follow the example https://music.yandex.ru/artist/0000000/tracks")
+except requests.exceptions.RequestException as e:
+    logger.error(e)
